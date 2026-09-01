a489effaae2067c42f12acfad21c2f40b14f6e85
macadanj-coder <joshryanmac@hotmail.com>
2026-08-31 23:26:08 -0400

    feat: added classes for Board and Keyboard

--- tui.py
+++ tui.py
@@ -1,15 +1,24 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.containers import Vertical, Horizontal
+from textual.widgets import Footer, Header, Button
 
 
+class Keyboard(Vertical):
+    """A repesentation of the keyboard."""
+class Board(Vertical):
+
+    """A grid to represent the guesses entered"""
+    def compose(self) -> ComposeResult:
+        yield Button("PRESS ME!", id="button1")
 class Wordle(App):
-    """A Textual app to manage stopwatches."""
+    """A Textual app to play wordle."""
 
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
+        yield Board()
         yield Footer()
 
     def action_toggle_dark(self) -> None:
